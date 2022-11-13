08046958f08e4136c1945513e9325d38465114c1
gaabrieljesuss <gjs8@aluno.ifal.edu.br>
2022-11-12 21:52:06 -0500

    Add detail screen creation from json

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,22 +9,27 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail()
-                } label: {
-                    LandmarkRow(landmark: landmark)
+        if #available(iOS 16.0, *) {
+            NavigationView {
+                List(landmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
-            }
-            .navigationTitle("Landmarks")
-
+                .navigationTitle("Landmarks")
+            }.scrollContentBackground(.hidden)
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
